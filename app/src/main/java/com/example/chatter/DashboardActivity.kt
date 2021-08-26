258d45964159a6c6eaaddba51cf9011eb568548a
hd-agarwal <harshdhar2000@gmail.com>
2021-08-27 01:07:08 +0530

    Dashboard bottom navigation bug fixed

--- app/src/main/java/com/example/chatter/DashboardActivity.kt
+++ app/src/main/java/com/example/chatter/DashboardActivity.kt
@@ -1,11 +1,41 @@
 package com.example.chatter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.chatter.databinding.ActivityDashboardBinding
+
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        _binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        loadFragment(ChatsFragment())
+        _binding.dashboardBottomNav.setOnItemSelectedListener{ menuItem ->
+            val id = menuItem.itemId
+            if (id == R.id.miChats) {
+                loadFragment(ChatsFragment())
+                return@setOnItemSelectedListener true
+            } else if (id == R.id.miGroups) {
+                loadFragment(GroupChatsFragment())
+                return@setOnItemSelectedListener true
+            } else if (id == R.id.miContacts) {
+                loadFragment(ContactsFragment())
+                return@setOnItemSelectedListener true
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment?) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        if (fragment != null) {
+            transaction.replace(R.id.frame_container, fragment)
+        }
+        transaction.commit()
     }
 }

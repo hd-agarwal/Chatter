8594e15df27908599edbfa69e76b2bc91309eadd
hd-agarwal <harshdhar2000@gmail.com>
2021-08-26 20:38:27 +0530

    Dashboard bottom navigation added

--- app/src/main/java/com/example/chatter/models/User.kt
+++ app/src/main/java/com/example/chatter/models/User.kt
@@ -4,5 +4,7 @@ class User (
     var username: String? = null,
     var phone_number: String? = null,
     var status: String? = null,
-    var photo_url: String? = null
+    var photo_url: String? = null,
+    var groups:Array<String>?=null,
+    var chats:Array<String>?=null
 )

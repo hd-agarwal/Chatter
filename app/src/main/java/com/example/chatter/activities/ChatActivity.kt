60d70f4130c3284c4f7ed8dd78d859c3b2f218ff
hd-agarwal <harshdhar2000@gmail.com>
2021-09-05 17:59:19 +0530

    Chat layout; Redundant files

--- app/src/main/java/com/example/chatter/activities/ChatActivity.kt
+++ app/src/main/java/com/example/chatter/activities/ChatActivity.kt
@@ -2,14 +2,36 @@ package com.example.chatter.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.chatter.R
+import com.vanniktech.emoji.ios.IosEmojiProvider
+import com.vanniktech.emoji.EmojiManager
+import kotlinx.android.synthetic.main.activity_chat.*
 
-const val NAME="name"
-const val UID="uid"
-const val IMAGE_URL="imageUrl"
+const val NAME = "name"
+const val UID = "uid"
+const val IMAGE_URL = "imageUrl"
+
 class ChatActivity : AppCompatActivity() {
+    private val friendId by lazy {
+        intent.getStringExtra(UID)
+    }
+    private val friendName by lazy {
+        intent.getStringExtra(NAME)
+    }
+    private val friendImageUrl by lazy {
+        intent.getStringExtra(IMAGE_URL)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        EmojiManager.install(IosEmojiProvider())
         setContentView(R.layout.activity_chat)
+        tvUsername.apply{
+            text=friendName
+        }
+        Glide.with(this).load(friendImageUrl)
+            .placeholder(R.drawable.ic_default_profile_foreground)
+            .into(ivUserPhoto)
     }
 }

6148de2bb496b2f28d21a295128f2aa3da37a53a
hd-agarwal <harshdhar2000@gmail.com>
2021-09-05 23:09:25 +0530

    Inbox modal added

---
+++ app/src/main/java/com/example/chatter/models/Inbox.kt
@@ -0,0 +1,14 @@
+package com.example.chatter.models
+
+import java.util.*
+
+data class Inbox(
+    val message: String,
+    val from: String,
+    var name: String,
+    var imageUrl: String,
+    val time: Date,
+    var count: Int
+) {
+    constructor() : this("", "", "", "", Date(), 0)
+}
